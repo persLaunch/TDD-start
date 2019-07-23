84ac503c8176adbf4ae56482bfbf83e324340c79
tomguillermou <tomguillermou@gmail.com>
2019-07-23 13:27:51 +0200

    Add a route to get users

--- src/routes/api.test.ts
+++ src/routes/api.test.ts
@@ -1,12 +1,35 @@
 import request from "supertest";
 import app from "../app";
 
-describe("API", () => {
+describe("Routes", () => {
 
-  describe("Routes", () => {
+  describe("API", () => {
 
-    test("Undefined route should return a 404 error", () => {
-      request(app).get("/a-route-that-does-not-exist").expect(404);
+    describe("Generic", () => {
+
+      describe("GET /undefined-route", () => {
+        it("Should return a 404 error", () => {
+          return request(app)
+            .get("/undefined-route")
+            .expect(404);
+        });
+      });
+
     });
+
+    describe("Users", () => {
+
+      describe("GET /users", () => {
+        it("Should return JSON with status 200", () => {
+          return request(app)
+            .get("/users")
+            .expect("Content-Type", /json/)
+            .expect(200);
+        });
+      });
+
+    });
+
   });
+
 });
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const router = Router();
+
+router.get("/users", (req: Request, res: Response) => {
+  const users: string[] = [];
+
+  res.status(200).json({ users });
+});
 
 export default router;
