00861eea1e05aec0725bf86766d478c5509f037e
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-07-23 11:42:56 +0200

    Mongo Connection

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,4 +1,7 @@
-require("dotenv").config();
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 describe("Server", () => {
   describe("MongoDb_uri", () => {
@@ -20,6 +23,24 @@ describe("Server", () => {
 
       // Assert
       expect(process.env.MONGODB_DATABASE).not.toBeUndefined();
+    });
+  });
+
+  describe("MongoDb", () => {
+    it("should be connected", async () => {
+      // Arrange
+      const mongoDbUri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+      const mongoDbDatabase = process.env.MONGODB_DATABASE || "laranode";
+
+      // Act
+      await mongoose.connect(`${mongoDbUri}/${mongoDbDatabase}`, {
+        useNewUrlParser: true
+      });
+      const connectionState = await mongoose.connection.readyState;
+      mongoose.connection.close();
+
+      // Assert
+      expect(connectionState).toEqual(1);
     });
   });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
-
 import http from "http";
 
 import app from "./app";
 
+import mongoose from "mongoose";
+
 const port = process.env.SERVER_PORT || 3000;
+const mongoDbUri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const mongoDbDatabase = process.env.MONGODB_DATABASE || "laranode";
+
+mongoose.connect(
+  `${mongoDbUri}/${mongoDbDatabase}`,
+  { useNewUrlParser: true },
+  (err: any) => {
+    if (err) {
+      console.log(`Error trying to connect to db: ${mongoDbDatabase}`);
+      console.log(err);
+    } else {
+      console.log(`Connected to db: ${mongoDbDatabase}`);
+    }
+  }
+);
 
 const server = http.createServer(app);
 
